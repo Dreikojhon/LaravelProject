3e88c47477ac9482772732c35edb42894c2ec0e1
Jhon_Dreiko <jhoncespedesramos@gmail.com>
2019-05-27 11:09:21 -0500

    Estilo al formulario de creacion añadido

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,6 +6,9 @@
 
     @if ($errors->any())
     <div class="alert-danger">Existen errores en el formulario
+        @if ($errors->has('name'))
+        <li>{{ $errors->first('name') }}</li>
+        @endif
         {{-- <ul>
             @foreach($errors->all() as $error)
                 <li>{{ $error }}</li>
@@ -16,25 +19,21 @@
 
     <form method="POST" action="{{ url('usuarios') }}">
         {{ csrf_field() }}
-        <label for="name">Ingrese los datos para ingresar un nuevo usuario</label><br>
-        <input type="text" name="name" placeholder="Nombre" value="{{ old('name') }}">
-        @if ($errors->has('name'))
-            <p>{{ $errors->first('name') }}</p>
-        @endif
-        <br>
-        <input type="email" name="email" placeholder="Correo Electrónico" value="{{ old('email') }}"><br>
-        <input type="password" name="password" placeholder="Clave de Usuario">
-
-
-
-
-
-        <button type="submit">Crear Usuario</button>
-    
+        <div class="form-group">
+            <label for="name">Ingrese los datos para crear un nuevo usuario</label><br>
+            <input type="name" class="form-control" name="name" placeholder="Nombre" value="{{ old('name') }}">
+            
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" name="email"  placeholder="Correo Electrónico" value="{{ old('email') }}">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" name="password" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Crear Usuario</button>
     </form>
-   
-
-
 
     <p>
         <a href="{{route('users.index')}}">Regresar al listado de usuarios</a>
